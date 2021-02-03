ab451601b99412dc492b69f5a156039430d8ae90
PaulDelestre <71083544+PaulDelestre@users.noreply.github.com>
2021-02-03 19:01:17 +0100

    profil utilisateur

--- migrations/Version20210203162131.php
+++ migrations/Version20210203162131.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210203094220 extends AbstractMigration
+final class Version20210203162131 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,14 @@ final class Version20210203094220 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users ADD is_verified TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE annonces CHANGE created_at created_at DATETIME NOT NULL');
+        $this->addSql('ALTER TABLE users ADD name VARCHAR(100) NOT NULL, ADD firstname VARCHAR(100) NOT NULL');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE users DROP is_verified');
+        $this->addSql('ALTER TABLE annonces CHANGE created_at created_at DATETIME DEFAULT CURRENT_TIMESTAMP');
+        $this->addSql('ALTER TABLE users DROP name, DROP firstname');
     }
 }
