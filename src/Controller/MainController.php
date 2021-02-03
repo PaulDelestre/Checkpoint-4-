7499f5bce619655a945ad6edbe10b5f987ba3b3d
PaulDelestre <71083544+PaulDelestre@users.noreply.github.com>
2021-02-03 14:52:14 +0100

    gestion utilistor, inscription , conexion ,password ,bdd

---
+++ src/Controller/MainController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home")
+     */
+    public function index(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+
+    /**
+     * @Route("/mentions-legales", name="mentions")
+     */
+    public function mentions(): Response
+    {
+        return $this->render('main/mentions.html.twig');
+    }
+}
